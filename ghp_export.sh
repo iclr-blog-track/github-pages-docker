6f8516aeed384b89d3397e9ad0512b17de8ffb93
Charlie Gauthier <charlie.gauthier@umontreal.ca>
2021-11-30 16:53:33 -0500

    fixed zip script

--- ghp_export.sh
+++ ghp_export.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-jekyll clean && jekyll build
+
+cp /srv/jekyll/_config.yml /srv/jekyll/_config.yml.bak
+GHP_UUID="https://iclr.iro.umontreal.ca/$(uuidgen)_$(date +%s)" yq e -i '.url = strenv(GHP_UUID)' /srv/jekyll/_config.yml
+
+jekyll clean
+jekyll build
 rm -rf site.zip
 zip -r site.zip _site
 chmod 777 site.zip
+
+#rm /srv/jekyll/_config.yml
+#mv /srv/jekyll/_config.yml.bak /srv/jekyll/_config.yml
+chmod 777 _config.yml
